2e129fa45a070fb184295b73ebbcf97c6c54208b
Chris Bartkewicz <christopher@irreverentwarriors.com>
2017-08-04 15:05:56 -0600

    MakeChange2.1

--- src/MCProject.java
+++ src/MCProject.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class MCProject {
 
+	DecimalFormat money = new DecimalFormat("00.00");
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
 	}
 
 }
